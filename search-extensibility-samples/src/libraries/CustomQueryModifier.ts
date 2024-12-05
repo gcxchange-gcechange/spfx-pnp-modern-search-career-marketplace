cf6b605c2cf6b655f3191216106c2a0716d9216c
doug0102 <shea.wow@hotmail.com>
2024-12-05 10:29:16 -0500

    switch to using session storage for advanced search

--- search-extensibility-samples/src/libraries/CustomQueryModifier.ts
+++ search-extensibility-samples/src/libraries/CustomQueryModifier.ts
@@ -5,6 +5,8 @@ import { Globals, Language } from "./Globals";
 
 export interface IAdvancedSearchQueryModifierProperties {
   searchBoxSelector: string;
+  searchButtonId: string;
+  clearButtonId: string;
   jobTitleId: string;
   departmentId: string;
   classificationCodeId: string;
@@ -17,152 +19,95 @@ export interface IAdvancedSearchQueryModifierProperties {
 //CustomQueryModifier
 export class AdvancedSearchQueryModifier extends BaseQueryModifier<IAdvancedSearchQueryModifierProperties> {
 
-  private jobTitle: string = '*';
-  private department: string = '*';
-  private classificationCode: string = '*';
-  private classificationLevel: string = '*';
-  private languageRequirement: string = '*';
-  private region: string = '*';
-  private duration: string = '*';
+  private static readonly DEFAULT_VALUE = '*';
+  private static readonly SESSION_STORAGE_KEYS = [
+    'gcx-cm-jobTitle',
+    'gcx-cm-departmentId',
+    'gcx-cm-classificationCodeId',
+    'gcx-cm-classificationLevelId',
+    'gcx-cm-languageRequirement',
+    'gcx-cm-regionId',
+    'gcx-cm-durationId'
+  ];
 
-  // TODO: Search when prop change
   public async onInit(): Promise<void> {
+    debugger;
+    AdvancedSearchQueryModifier.SESSION_STORAGE_KEYS.forEach(key => {
+      if (!sessionStorage.getItem(key)) {
+        sessionStorage.setItem(key, AdvancedSearchQueryModifier.DEFAULT_VALUE);
+      }
+    });
+    this.setupListeners();
+  }
+
+  private setupListeners(): void {
     // eslint-disable-next-line @typescript-eslint/no-this-alias
     const context = this;
 
-    if (this._properties.jobTitleId) {
-      let el = document.getElementById(this._properties.jobTitleId);
+    if (this._properties.searchButtonId) {
+      let el = document.getElementById(this._properties.searchButtonId);
       if (el) {
-        el.addEventListener('change', function(event) {
-          let target = event.target as HTMLInputElement;
-          context.jobTitle = target.value;
-          context.triggerSearch();
+        el.addEventListener('click', (event) => {
+          setTimeout(() => {
+            context.triggerSearch();
+          }, 0);
         });
-      }
-    }
+      } else { console.error(`Advanced Search: Couldn't find advanced search button element with the ID \'${this._properties.searchButtonId}\'`); }
+    } else { console.error(`Advanced Search: No ID provided for SearchButton`); }
 
-    if (this._properties.departmentId) {
-      let el = document.getElementById(this._properties.departmentId);
+    if (this._properties.clearButtonId) {
+      let el = document.getElementById(this._properties.clearButtonId);
       if (el) {
-        el.addEventListener('focusin', (event) => {
-          context.department = context.getElementText(context._properties.departmentId);
-          if (context.department != '*')
+        el.addEventListener('click', (event) => {
+          event.preventDefault();
+          setTimeout(() => {
             context.triggerSearch();
+          }, 0);
         });
-      }
-    }
-
-    if (this._properties.classificationCodeId) {
-      let el = document.getElementById(this._properties.classificationCodeId);
-      if (el) {
-        el.addEventListener('focusin', (event) => {
-          context.classificationCode = context.getElementText(context._properties.classificationCodeId);
-          if (context.classificationCode != '*')
-            context.triggerSearch();
-        });
-      }
-    }
-
-    if (this._properties.classificationLevelId) {
-      let el = document.getElementById(this._properties.classificationLevelId);
-      if (el) {
-        el.addEventListener('focusin', (event) => {
-          context.classificationLevel = context.getElementText(context._properties.classificationLevelId);
-          if (context.classificationLevel != '*')
-            context.triggerSearch();
-        });
-      }
-    }
-
-    if (this._properties.languageRequirementId) {
-      let el = document.getElementById(this._properties.languageRequirementId);
-      if (el) {
-        el.addEventListener('focusin', (event) => {
-          context.languageRequirement = context.getElementText(context._properties.languageRequirementId);
-          if (context.languageRequirement != '*')
-            context.triggerSearch();
-        });
-      }
-    }
-
-    if (this._properties.regionId) {
-      let el = document.getElementById(this._properties.regionId);
-      if (el) {
-        el.addEventListener('focusin', (event) => {
-          context.region = context.getElementText(context._properties.regionId);
-          if (context.region != '*')
-            context.triggerSearch();
-        });
-      }
-    }
-    
-    if (this._properties.durationId) {
-      let el = document.getElementById(this._properties.durationId);
-      if (el) {
-        el.addEventListener('focusin', (event) => {
-          context.duration = context.getElementText(context._properties.durationId);
-          if (context.duration != '*')
-            context.triggerSearch();
-        });
-      }
-    }
+      } else { console.error(`Advanced Search: Couldn't find advanced clear button element with the ID \'${this._properties.clearButtonId}\'`); }
+    } else { console.error(`Advanced Search: No ID provided for ClearButton`); }
   }
 
-  private getElementText(id: string): string | null {
-    let el = document.getElementById(id);
-    if (el) {
-      let retVal = el.innerText;
-
-      if (el.nodeName === 'INPUT')
-        retVal = (el as HTMLTextAreaElement).defaultValue;
-
-      if (retVal == '' || retVal == '')
-        retVal = '*';
-
-      return retVal.replace('\n', '');
-    }
-    return '*';
-  }
-
-  private triggerSearch() {
+  private triggerSearch(): void {
     if (this._properties.searchBoxSelector) {
-      let el = document.querySelector(this._properties.searchBoxSelector);
-      if (el) {
-        let searchBox = el as HTMLInputElement;
+        let el = document.querySelector(this._properties.searchBoxSelector);
+        if (el) {
+            let searchBox = el as HTMLInputElement;
 
-        if (searchBox.defaultValue == "") {
-          searchBox.value = " ";
-          searchBox.dispatchEvent(new Event('input', { bubbles: true }));
+            if (searchBox.defaultValue === "") {
+                searchBox.value = " ";
+                searchBox.dispatchEvent(new Event('input', { bubbles: true }));
+            }
+
+            el.dispatchEvent(new KeyboardEvent('keydown', {
+                key: 'Enter',
+                code: 'Enter',
+                keyCode: 13,
+                which: 13,
+                bubbles: true,
+                cancelable: true,
+            }));
         }
-
-        el.dispatchEvent(new KeyboardEvent('keydown', {
-          key: 'Enter',
-          code: 'Enter',
-          keyCode: 13,
-          which: 13,
-          bubbles: true,
-          cancelable: true 
-        }));
-      }
     }
   }
 
   public async modifyQuery(queryText: string): Promise<string> {
-    queryText = queryText === undefined ? '*' : queryText;
+    queryText = queryText || AdvancedSearchQueryModifier.DEFAULT_VALUE;
 
-    this.jobTitle = this._properties.jobTitleId ? this.getElementText(this._properties.jobTitleId) : '*';
-    this.department = this._properties.departmentId ? this.getElementText(this._properties.departmentId) : '*';
-    this.classificationCode = this._properties.classificationCodeId ? this.getElementText(this._properties.classificationCodeId) : '*';
-    this.classificationLevel = this._properties.classificationLevelId ? this.getElementText(this._properties.classificationLevelId) : '*';
-    this.languageRequirement = this._properties.languageRequirementId ? this.getElementText(this._properties.languageRequirementId) : '*';
-    this.region = this._properties.regionId ? this.getElementText(this._properties.regionId) : '*';
-    this.duration = this._properties.durationId ? this.getElementText(this._properties.durationId) : '*';
+    const jobTitle = sessionStorage.getItem('gcx-cm-jobTitle') || AdvancedSearchQueryModifier.DEFAULT_VALUE;
+    const department = sessionStorage.getItem('gcx-cm-departmentId') || AdvancedSearchQueryModifier.DEFAULT_VALUE;
+    const classificationCode = sessionStorage.getItem('gcx-cm-classificationCodeId') || AdvancedSearchQueryModifier.DEFAULT_VALUE;
+    const classificationLevel = sessionStorage.getItem('gcx-cm-classificationLevelId') || AdvancedSearchQueryModifier.DEFAULT_VALUE;
+    const languageRequirement = sessionStorage.getItem('gcx-cm-languageRequirementId') || AdvancedSearchQueryModifier.DEFAULT_VALUE;
+    // TODO: Figure out how we are doing location/region
+    const region = sessionStorage.getItem('gcx-cm-regionId') || AdvancedSearchQueryModifier.DEFAULT_VALUE;
+    const duration = sessionStorage.getItem('gcx-cm-durationId') || AdvancedSearchQueryModifier.DEFAULT_VALUE;
 
-    if (Globals.getLanguage() == Language.French) {
-      return `${queryText} path: https://devgcx.sharepoint.com/sites/CM-test/Lists/JobOpportunity/ contentclass: STS_ListItem_GenericList "CM-JobTitleFr":*${this.jobTitle}* AND "CM-LanguageRequirement":${this.languageRequirement} AND "CM-Department":${this.department} AND "CM-ClassificationCode":${this.classificationCode} AND "CM-ClassificationLevel":${this.classificationLevel} AND "CM-Duration":${this.duration}`;
+    if (Globals.getLanguage() === Language.French) {
+      return `${queryText} path: https://devgcx.sharepoint.com/sites/CM-test/Lists/JobOpportunity/ contentclass: STS_ListItem_GenericList "CM-JobTitleFr":*${jobTitle}* AND "CM-LanguageRequirementId":${languageRequirement} AND "CM-DepartmentId":${department} AND "CM-ClassificationCodeId":${classificationCode} AND "CM-ClassificationLevelId":${classificationLevel} AND "CM-DurationId":${duration}`;
+    } else {
+      return `${queryText} path: https://devgcx.sharepoint.com/sites/CM-test/Lists/JobOpportunity/ contentclass: STS_ListItem_GenericList "CM-JobTitleEn":*${jobTitle}* AND "CM-LanguageRequirementId":${languageRequirement} AND "CM-DepartmentId":${department} AND "CM-ClassificationCodeId":${classificationCode} AND "CM-ClassificationLevelId":${classificationLevel} AND "CM-DurationId":${duration}`;
     }
-    else 
-      return `${queryText} path: https://devgcx.sharepoint.com/sites/CM-test/Lists/JobOpportunity/ contentclass: STS_ListItem_GenericList "CM-JobTitleEn":*${this.jobTitle}* AND "CM-LanguageRequirement":${this.languageRequirement} AND "CM-Department":${this.department} AND "CM-ClassificationCode":${this.classificationCode} AND "CM-ClassificationLevel":${this.classificationLevel} AND "CM-Duration":${this.duration}`;
   }
 
   // TODO: Update listeners
@@ -170,19 +115,9 @@ export class AdvancedSearchQueryModifier extends BaseQueryModifier<IAdvancedSear
     switch(propertyPath) {
       case 'queryModifierProperties.searchBoxSelector':
         break;
-      case 'queryModifierProperties.jobTitleId':
+      case 'queryModifierProperties.searchButton':
         break;
-      case 'queryModifierProperties.departmentId':
-        break;
-      case 'queryModifierProperties.classificationCodeId':
-        break;
-      case 'queryModifierProperties.classificationLevelId':
-        break;
-      case 'queryModifierProperties.languageRequirementId':
-        break;
-      case 'queryModifierProperties.regionId':
-        break;
-      case 'queryModifierProperties.durationId':
+      case 'queryModifierProperties.clearButton':
         break;
     }
   }
@@ -198,40 +133,15 @@ export class AdvancedSearchQueryModifier extends BaseQueryModifier<IAdvancedSear
             description: 'CSS selector for the pnp search input.',
             placeholder: '[data-sp-feature-tag="pnpSearchBoxWebPart web part (PnP - Search Box)"] input',
           }),
-          PropertyPaneTextField('queryModifierProperties.jobTitleId', {
-            label: 'JobTitle ID',
-            description: 'The ID of the HTML element containing the job title text box.',
-            placeholder: 'txtJobTitle',
+          PropertyPaneTextField('queryModifierProperties.searchButtonId', {
+            label: 'Advanced Search - Search Button ID',
+            description: 'The ID of the advanced search\'s search button.',
+            placeholder: 'advancedSearch-Search',
           }),
-          PropertyPaneTextField('queryModifierProperties.departmentId', {
-            label: 'Department ID',
-            description: 'The ID of the HTML element containing the department drop down.',
-            placeholder: 'ddDepartment',
-          }),
-          PropertyPaneTextField('queryModifierProperties.classificationCodeId', {
-            label: 'ClassificationCode ID',
-            description: 'The ID of the HTML element containing the classification code drop down.',
-            placeholder: 'ddClassificationCode',
-          }),
-          PropertyPaneTextField('queryModifierProperties.classificationLevelId', {
-            label: 'ClassificationLevel ID',
-            description: 'The ID of the HTML element containing the classification level drop down.',
-            placeholder: 'ddClassificationLevel',
-          }),
-          PropertyPaneTextField('queryModifierProperties.languageRequirementId', {
-            label: 'LanguageRequirement ID',
-            description: 'The ID of the HTML element containing the language requirement drop down.',
-            placeholder: 'ddLanguageRequirement',
-          }),
-          PropertyPaneTextField('queryModifierProperties.regionId', {
-            label: 'Region ID',
-            description: 'The ID of the HTML element containing the region drop down.',
-            placeholder: 'ddRegion',
-          }),
-          PropertyPaneTextField('queryModifierProperties.durationId', {
-            label: 'Duration ID',
-            description: 'The ID of the HTML element containing the duration drop down.',
-            placeholder: 'ddDuration',
+          PropertyPaneTextField('queryModifierProperties.clearButtonId', {
+            label: 'Advanced Search - Clear Button ID',
+            description: 'The ID of the advanced search\'s clear button.', 
+            placeholder: 'advancedSearch-Clear',
           })
         ],
       },
