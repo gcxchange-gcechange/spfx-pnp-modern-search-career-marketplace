509b1c531c8212a021f8db0da8bf092cb515c767
doug0102 <shea.wow@hotmail.com>
2026-06-03 12:39:27 -0400

    trigger pipeline

--- search-extensibility-samples/src/libraries/SelectLanguage.ts
+++ search-extensibility-samples/src/libraries/SelectLanguage.ts
@@ -2,7 +2,7 @@ import { Language } from './Globals';
 const english = require("./myCompanyLibrary/loc/en-us.js")
 const french = require("./myCompanyLibrary/loc/fr-fr.js")
 
-export function SelectLanguage(lang: Language): IMyCompanyLibraryLibraryStrings  {
+export function SelectLanguage(lang: Language): IMyCompanyLibraryLibraryStrings {
     switch (lang) {
         case Language.English: {
             return english;
